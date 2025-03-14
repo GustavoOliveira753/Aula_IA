d7d989009de57125121267100c1b6d77047d34f8
GustavoOliveira753 <ggu865220@gmail.com>
2025-03-14 08:42:57 -0300

    Exercicio 1 (completo)

--- Aula_4/exercicios/exercicio1.py
+++ Aula_4/exercicios/exercicio1.py
@@ -1,17 +1,8 @@
 validacao = "ATCG"
+complementar = {"A": "T", "T": "A", "C": "G", "G": "C"}
 cadeia = input("Insira uma cadeia de DNA: ").upper()
-cadeiac = ""
-# cadeiaf = "".join(filter(lambda letra: letra in validacao, cadeia))
-cadeiaf = ''.join([char for char in cadeia if char in validacao])
-for char in cadeiaf:
-    if char in "A":
-        cadeiac = "".join("T")
-    if char in "T":
-        cadeiac = "".join("A")
-    if char in "C":
-        cadeiac = "".join("G")
-    if char in "G":
-        cadeiac = "".join("C")
-
-print("Cadeia original formatada:", cadeiaf)
+# cadeia = "".join(filter(lambda letra: letra in validacao, cadeia))
+cadeia = "".join(char for char in cadeia if char in validacao)
+cadeiac = "".join(complementar[char] for char in cadeia)
+print("Cadeia original formatada:", cadeia)
 print("Cadeia complementar:", cadeiac)
