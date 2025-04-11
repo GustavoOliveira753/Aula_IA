3bf93ecc7f410a1161ae01f3cb87242618d261c7
GustavoOliveira753 <ggu865220@gmail.com>
2025-04-11 09:00:25 -0300

    terminado

--- Aula_6/exercicios/exercicio.py
+++ Aula_6/exercicios/exercicio.py
@@ -1,7 +1,12 @@
 def calcular_total_venda(linhas):
+    for linha in linhas:
+        yield int(linha[1])*float(linha[2])
     
 arquivo = open("Aula_6/exercicios/vendas.txt", 'r')
 linhas = arquivo.readlines()
 linhas = [linha.strip("\n") for linha in linhas]
 linhasArr = [linha.split(";") for linha in linhas]
-print(linhasArr)
+totalVendas = round(sum(calcular_total_venda(linhasArr)), 2)
+for linha in linhasArr:
+    print(f"Produto: {linha[0]} - Total: R$ {round((int(linha[1]) * float(linha[2])) ,2)}")
+print(f"\nTotal Geral: R$ {totalVendas}")
